5cf37f91ddc794da28177018ab9d24e372184456
t4l3ntzce <153099402+t4l3ntzce@users.noreply.github.com>
2023-12-30 22:16:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Task03.Controllers;
 
 namespace Task03
 {
@@ -12,16 +10,13 @@ namespace Task03
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllers();
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
